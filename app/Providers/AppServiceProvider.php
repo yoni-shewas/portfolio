70f91cc390f4d2c719d3835e1f80822988053e35
yoni-shewas <yonatanmekuriya@gmail.com>
2024-12-27 08:22:08 +0300

    Hot fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Foundation\Vite;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -20,20 +21,18 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+    
+
     public function boot()
     {
         Log::info('Debug message', ['context' => []]);
-        Log::debug('Debug message', ['context' => []]);
-        Log::warning('Debug message', ['context' => []]);
-        Log::error('Debug message', ['context' => []]);
-        Log::critical('Debug message', ['context' => []]);
-        Log::alert('Debug message', ['context' => []]);
-        Log::emergency('Debug message', ['context' => []]);
     
-        // Ensure Vite::prefetch is correctly called
-        if (class_exists(\Illuminate\Foundation\Vite::class)) {
-            \Illuminate\Foundation\Vite::prefetch(concurrency: 3);
+        // Create an instance of Vite
+        if (class_exists(Vite::class)) {
+            $vite = app(Vite::class); 
+            $vite->prefetch(concurrency: 3); 
         }
     }
+    
     
 }
